8c057f770ac8fa75cc301f167580589900851122
GeraldineSaco <gsacoruiz@gmail.com>
2025-12-04 23:42:34 +0100

    deleted comments file vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    // ➡️ ACTIVA JSDOM AQUÍ
     environment: 'jsdom',
-    // Opcional: Esto hace que 'describe', 'test', 'expect' estén disponibles globalmente sin importarlos
     globals: true, 
   },
 });
-
-
